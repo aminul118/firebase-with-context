1a12f9c24d7caf07c59b452362e10b187c426d21
Md Aminul Islam <mr.aminul118@gmail.com>
2024-11-11 07:50:54 +0600

    Router dom error solve

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { route } from "./routes/route";
-
+import AuthProvider from "./providers/AuthProvider";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={route} />
+    <AuthProvider>
+      <RouterProvider
+        future={{
+          v7_startTransition: true,
+        }}
+        router={route}
+      />
+    </AuthProvider>
   </StrictMode>
 );
--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -4,25 +4,38 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 
-const route = createBrowserRouter([
+const route = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/login",
+          element: <Login />,
+        },
+        {
+          path: "/register",
+          element: <Register />,
+        },
+      ],
+    },
+  ],
+  
   {
-    path: "/",
-    element: <MainLayout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-      },
-      {
-        path: "/register",
-        element: <Register />,
-      },
-    ],
-  },
-]);
+    future: {
+      v7_fetcherPersist: true,
+      v7_relativeSplatPath: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_skipActionErrorRevalidation: true, 
+      v7_startTransition: true, 
+    },
+  }
+);
 
 export { route };
